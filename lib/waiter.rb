84fa56da589879b0df517c4c9c73212f6f5305a9
Arvin Fernandez <arvinf07@gmail.com>
2020-11-09 14:13:57 -0500

    commit tree 1e21a52bfe2cea3f9cfbf8465e19593d9056460d

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,34 @@
 class Waiter
 
+  attr_accessor :name, :years_of_exp
+  @@all = []
+
+  def initialize(name, years_of_exp)
+    @name = name
+    @years_of_exp = years_of_exp
+    save 
+  end  
+
+  def save
+    @@all << self
+  end  
+
+  def self.all
+    @@all
+  end  
+
+  def new_meal(customer, total, tip)
+    Meal.new(self, customer, total, tip)
+  end  
+
+  def meals
+    Meal.all.select {|meal| meal.waiter == self}
+  end  
+
+  def best_tipper
+    tips = meals.collect {|meal| meal.tip}
+    meal = meals.find {|meal| meal.tip == tips.sort.last}
+    meal.customer
+  end  
+
 end
